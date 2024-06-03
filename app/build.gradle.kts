ce680103442059b1db0aecda05578f33648a4bb8
iMAndDev <maksym.burym@proton.me>
2024-06-03 08:38:55 +0300

    Added new dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.dagger.hilt.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.kotlin.parcelize)
+    alias(libs.plugins.kotlin.serialization)
 }
 
 android {
@@ -8,6 +12,9 @@ android {
     compileSdk = 34
 
     defaultConfig {
+        versionCode = property("versCode") as? Int
+        versionName = (property("versName") as? String)?.replace("\"", "")
+
         applicationId = "ua.nure.maksymburym.eyesana"
         minSdk = 24
         targetSdk = 34
@@ -20,7 +27,15 @@ android {
         }
     }
 
+    buildFeatures {
+        buildConfig = true
+    }
+
     buildTypes {
+        configureEach {
+            buildConfigField("int", "VERSION_CODE", property("versCode") as String)
+            buildConfigField("String", "VERSION_NAME", property("versName") as String)
+        }
         release {
             isMinifyEnabled = false
             proguardFiles(
@@ -34,7 +49,7 @@ android {
         targetCompatibility = JavaVersion.VERSION_1_8
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
     }
     buildFeatures {
         compose = true
@@ -53,17 +68,41 @@ dependencies {
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.appcompat)
+
+    /** Compose */
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.material)
+    implementation(libs.androidx.material.icons.ext)
+
+    /** UI */
     implementation(libs.androidx.ui)
+    implementation(libs.androidx.fragment.ktx)
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.bundles.navigation)
+
+    /** Testing */
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.ui.test.junit4)
-    debugImplementation(libs.androidx.ui.tooling)
-    debugImplementation(libs.androidx.ui.test.manifest)
+
+    /** DI */
+    implementation(libs.hilt.android.base)
+    ksp(libs.hilt.compiler)
+
+    /** Crypto */
+    implementation(libs.androidx.security.crypto)
+
+    /** Serialization */
+    implementation(libs.kotlinx.serialization.json)
+
+    /** Logging */
+    implementation(libs.timber)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,8 @@
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
+    alias(libs.plugins.dagger.hilt.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.parcelize) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
 }
